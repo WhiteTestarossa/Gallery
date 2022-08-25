35af4fb3dbcbf36e589c1870cc5182551e4bec6a
WhiteTestarossa <daniel.belokursky@gmail.com>
2022-08-25 15:32:07 +0300

    feat: add basic UI

---
+++ Gallery/CollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewController.swift
+//  Gallery
+//
+//  Created by Daniel Belokursky on 25.08.22.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+    }
+
+}
--- Gallery/SceneDelegate.swift
+++ Gallery/SceneDelegate.swift
@@ -13,10 +13,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        
+        UITabBar.appearance().tintColor = UIColor.red
+        
+        window?.rootViewController = rootViewController()
+        window?.makeKeyAndVisible()
+    }
+    
+    func rootViewController() -> UIViewController {
+        
+        let tabBarController = UITabBarController()
+        let collectionVC = CollectionViewController()
+        collectionVC.tabBarItem = UITabBarItem(title: "Items", image: UIImage(systemName: "square.grid.2x2"), tag: 0)
+        
+        tabBarController.viewControllers = [collectionVC]
+        
+        return tabBarController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
